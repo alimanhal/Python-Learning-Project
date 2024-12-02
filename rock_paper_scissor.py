7d17ad182bfddf71dce21039257e4354717cb5f6
Ali Manhal <alimanhal@gmail.com>
2024-12-02 23:51:35 +0530

    Little bit more modified

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -1,7 +1,12 @@
 import random 
 
-emojis = { 'r':'🥌', 's':'✂', 'p':'📃'}
-choices = ('r', 'p','s')
+
+ROCK = 'r'
+SCISSORS = 's'
+PAPER = 'p'
+emojis = { ROCK:'🥌', SCISSORS:'✂', PAPER:'📃'}
+
+choices = tuple(emojis.keys())
 
 def get_user_choice():
     while True:
@@ -20,9 +25,9 @@ def determaine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
         print("Tie!!")
     elif (
-        (user_choice == 'r' and computer_choice == 's') or
-        (user_choice == 's' and computer_choice == 'p') or
-        (user_choice == 'p' and computer_choice == 'r')):
+        (user_choice == ROCK and computer_choice == SCISSORS) or
+        (user_choice == SCISSORS and computer_choice == PAPER) or
+        (user_choice == PAPER and computer_choice == ROCK)):
         print('You won!')
     else:
         print('You lose!')
